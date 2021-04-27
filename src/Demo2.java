a235e3b7c23681d45df863fc9199b74940005f61
qianhao <1771491417@qq.com>
2021-04-27 15:14:56 +0800

    Demo2修改

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,5 +1,6 @@
 public class Demo2 {
     public static void main(String[] args) {
-        System.out.println("sdgksl");
+        System.out.println("54566");
+        System.out.println("ggg");
     }
 }
