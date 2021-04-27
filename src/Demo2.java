eae60c870bfbaca10b599dcf416ced52a3c98ac1
qianhao <1771491417@qq.com>
2021-04-27 15:16:12 +0800

    Demo2修改

--- src/Demo2.java
+++ src/Demo2.java
@@ -2,5 +2,6 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("54566");
         System.out.println("ggg");
+        System.out.println("YYY");
     }
 }
